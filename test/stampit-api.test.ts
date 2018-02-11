a071d30304756b6bed9598183760dca15af198fe
向松涛 <280304286@163.com>
2018-02-11 17:14:44 +0800

    chore: Add a test to the propertyDescriptors.

--- test/stampit-api.test.ts
+++ test/stampit-api.test.ts
@@ -57,10 +57,43 @@ test('multiple arguments stampit(arg1, arg2, ...)', () => {
   expect(stampit(null, {deepConf: {x: 2}}).compose.deepConfiguration.x).toBe(2);
   expect(stampit(
     null,
-    {propertyDescriptors: {x: {writable: true}}}
+    {
+      propertyDescriptors: {
+        x: {
+          writable: true
+        }
+      }
+    }
   ).compose.propertyDescriptors).toEqual({x: {writable: true}});
   expect(stampit(
     null,
     {staticPropertyDescriptors: {x: {writable: true}}}
   ).compose.staticPropertyDescriptors).toEqual({x: {writable: true}});
+});
+
+test('test propertyDescriptors', () => {
+  const obj = stampit(
+    null,
+    {
+      propertyDescriptors: {
+        'property1': {
+          value: true,
+          writable: true
+        },
+        'property2': {
+          value: 'Hello',
+          writable: false
+        }
+      }
+    }
+  ).create();
+
+  expect(obj.property1).toBeTruthy();
+  obj.property1 = false;
+  expect(obj.property1).toBeFalsy();
+
+  expect(obj.property2).toBe('Hello');
+  expect(() => {
+    obj.property2 = 'World'
+  }).toThrow(TypeError);
 });
