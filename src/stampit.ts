a519e1b76d57f0ebe1e04049af650938b815c8a3
向松涛 <280304286@163.com>
2018-02-11 23:32:48 +0800

    fix: code clean

--- src/stampit.ts
+++ src/stampit.ts
@@ -1,63 +1,58 @@
-'use strict';
-import {isArray, isObject, isStamp} from './lib/utils'
+'use strict'
+import { isArray, isObject, isStamp } from './lib/utils'
 import assign from 'lodash/assign'
 import standardiseDescriptor from './lib/standardiseDescriptor'
 import compose from './lib/compose'
 import allUtilities from './lib/allUtilities'
-import {COMPOSE, COMPOSERS, CREATE, STATIC_PROPERTIES} from "./lib/const";
-import {Descriptor, Stamp} from "./lib/interface";
+import { COMPOSE, COMPOSERS, CREATE, STATIC_PROPERTIES } from './lib/const'
+import { Descriptor, Stamp } from './lib/interface'
 
-let stampitWithUtils: any;
+let stampitWithUtils: any
 
 /**
  * Infected stamp. Used as a storage of the infection metadata
  * @type {Function}
  * @return {Stamp}
  */
-let baseStampit: Stamp; // temporary reusing the variable
+let baseStampit: Stamp // temporary reusing the variable
 
-function stampit(this: (undefined | Stamp), ...rest: (Descriptor | Stamp)[]): Stamp {
-  let composables: (Descriptor | Stamp)[] = [];
-  let composerResult = this;
+function stampit(this: undefined | Stamp, ...rest: (Descriptor | Stamp)[]): Stamp {
+  let composables: (Descriptor | Stamp)[] = []
+  let composerResult = this
 
   for (let i = 0; i < rest.length; i++) {
-    let tmp: any = rest[i];
+    let tmp: any = rest[i]
     if (isObject(tmp)) {
-      composables.push(isStamp(tmp) ? tmp : standardiseDescriptor(tmp));
+      composables.push(isStamp(tmp) ? tmp : standardiseDescriptor(tmp))
     }
   }
 
   if (!baseStampit) {
-    let tmp: any = {};
-    allUtilities[CREATE] = function (this: Stamp, ...rest: any[]) {
-      return this.apply(undefined, rest);
-    };
-    tmp[STATIC_PROPERTIES] = allUtilities;
-    baseStampit = compose(tmp);
+    let tmp: any = {}
+    allUtilities[CREATE] = function(this: Stamp, ...rest: any[]) {
+      return this.apply(undefined, rest)
+    }
+    tmp[STATIC_PROPERTIES] = allUtilities
+    baseStampit = compose(tmp)
   }
 
   // Calling the standard pure compose function here.
-  let composable: Stamp = compose.apply(composerResult || baseStampit, composables);
-  if (composerResult) composables.unshift(composerResult);
+  let composable: Stamp = compose.apply(composerResult || baseStampit, composables)
+  if (composerResult) composables.unshift(composerResult)
 
-  let composers = composable[COMPOSE][COMPOSERS];
+  let composers = composable[COMPOSE][COMPOSERS]
   if (isArray(composers)) {
     for (let i = 0; i < composers.length; i++) {
-      composerResult = composers[i]({stamp: composable, composables: composables});
-      composable = isStamp(composerResult) ? composerResult as Stamp : composable;
+      composerResult = composers[i]({ stamp: composable, composables: composables })
+      composable = isStamp(composerResult) ? (composerResult as Stamp) : composable
     }
   }
 
-  return composable;
+  return composable
 }
 
-/**
- * Infected compose
- * Parameters:  {...Composable} The list of composables.
- * @return {Stamp} The Stampit-flavoured stamp
- */
-stampitWithUtils = allUtilities[COMPOSE] = assign(stampit, allUtilities); // Setting up the shortcut functions
+stampitWithUtils = allUtilities[COMPOSE] = assign(stampit, allUtilities) // Setting up the shortcut functions
 
-stampitWithUtils[COMPOSE] = stampitWithUtils.bind(undefined); // bind to undefined
+stampitWithUtils[COMPOSE] = stampitWithUtils.bind(undefined) // bind to undefined
 
 export default stampitWithUtils
