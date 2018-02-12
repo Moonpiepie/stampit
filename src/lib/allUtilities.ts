f10bd83ae747643b65e04c22db3e0a92fa880bc8
向松涛 <280304286@163.com>
2018-02-12 17:12:28 +0800

    fix: Remove arguments statement

--- src/lib/allUtilities.ts
+++ src/lib/allUtilities.ts
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 import {
   COMPOSE,
   COMPOSERS,
@@ -19,50 +19,61 @@ import {
   STATIC_PROPERTIES,
   STATIC_PROPERTY_DESCRIPTORS,
   STATICS
-} from './const';
-import merge from 'lodash/merge';
-import extractUniqueFunctions from './extractUniqueFunctions';
-import assign from 'lodash/assign';
-import {concat} from './utils';
-import {Stamp, Stampit, Utilities} from './interface';
+} from './const'
+import merge from 'lodash/merge'
+import extractUniqueFunctions from './extractUniqueFunctions'
+import assign from 'lodash/assign'
+import { concat } from './utils'
+import { Stamp, Stampit, Utilities } from './interface'
 
-let allUtilities: any = {};
+let allUtilities: any = {}
 
 function createUtilityFunction(propName: string, action: Function) {
-  return function (this: (undefined | Stamp | Stampit)) {
-    let tmp: any = {};
-    tmp[propName] = action.apply(undefined, concat.apply([{}], arguments));
-    return ((this && (this as Stamp)[COMPOSE]) || allUtilities[COMPOSE]).call(this, tmp);
-  };
+  return function(this: undefined | Stamp | Stampit, ...rest: any[]) {
+    let tmp: any = {}
+    tmp[propName] = action.apply(undefined, concat.apply([{}], rest))
+    return ((this && (this as Stamp)[COMPOSE]) || allUtilities[COMPOSE]).call(this, tmp)
+  }
 }
 
-allUtilities[METHODS] = createUtilityFunction(METHODS, assign);
+allUtilities[METHODS] = createUtilityFunction(METHODS, assign)
 
-allUtilities[PROPERTIES] = allUtilities[PROPS] =
-  createUtilityFunction(PROPERTIES, assign);
+allUtilities[PROPERTIES] = allUtilities[PROPS] = createUtilityFunction(PROPERTIES, assign)
 
-allUtilities[INITIALIZERS] = allUtilities[INIT] =
-  createUtilityFunction(INITIALIZERS, extractUniqueFunctions);
+allUtilities[INITIALIZERS] = allUtilities[INIT] = createUtilityFunction(
+  INITIALIZERS,
+  extractUniqueFunctions
+)
 
-allUtilities[COMPOSERS] = createUtilityFunction(COMPOSERS, extractUniqueFunctions);
+allUtilities[COMPOSERS] = createUtilityFunction(COMPOSERS, extractUniqueFunctions)
 
-allUtilities[DEEP_PROPERTIES] = allUtilities[DEEP_PROPS] =
-  createUtilityFunction(DEEP_PROPERTIES, merge);
+allUtilities[DEEP_PROPERTIES] = allUtilities[DEEP_PROPS] = createUtilityFunction(
+  DEEP_PROPERTIES,
+  merge
+)
 
-allUtilities[STATIC_PROPERTIES] = allUtilities[STATICS] =
-  createUtilityFunction(STATIC_PROPERTIES, assign);
+allUtilities[STATIC_PROPERTIES] = allUtilities[STATICS] = createUtilityFunction(
+  STATIC_PROPERTIES,
+  assign
+)
 
-allUtilities[STATIC_DEEP_PROPERTIES] = allUtilities[DEEP_STATICS] =
-  createUtilityFunction(STATIC_DEEP_PROPERTIES, merge);
+allUtilities[STATIC_DEEP_PROPERTIES] = allUtilities[DEEP_STATICS] = createUtilityFunction(
+  STATIC_DEEP_PROPERTIES,
+  merge
+)
 
-allUtilities[CONFIGURATION] = allUtilities[CONF] =
-  createUtilityFunction(CONFIGURATION, assign);
+allUtilities[CONFIGURATION] = allUtilities[CONF] = createUtilityFunction(CONFIGURATION, assign)
 
-allUtilities[DEEP_CONFIGURATION] = allUtilities[DEEP_CONG] =
-  createUtilityFunction(DEEP_CONFIGURATION, merge);
+allUtilities[DEEP_CONFIGURATION] = allUtilities[DEEP_CONG] = createUtilityFunction(
+  DEEP_CONFIGURATION,
+  merge
+)
 
-allUtilities[PROPERTY_DESCRIPTORS] = createUtilityFunction(PROPERTY_DESCRIPTORS, assign);
+allUtilities[PROPERTY_DESCRIPTORS] = createUtilityFunction(PROPERTY_DESCRIPTORS, assign)
 
-allUtilities[STATIC_PROPERTY_DESCRIPTORS] = createUtilityFunction(STATIC_PROPERTY_DESCRIPTORS, assign);
+allUtilities[STATIC_PROPERTY_DESCRIPTORS] = createUtilityFunction(
+  STATIC_PROPERTY_DESCRIPTORS,
+  assign
+)
 
-export default (allUtilities as Utilities);
+export default allUtilities as Utilities
--- src/lib/createFactory.ts
+++ src/lib/createFactory.ts
@@ -1,45 +1,55 @@
-'use strict';
-import {COMPOSE, DEEP_PROPERTIES, INITIALIZERS, METHODS, PROPERTIES, PROPERTY_DESCRIPTORS} from './const';
-import {isFunction} from './utils';
-import merge from 'lodash/merge';
-import assign from 'lodash/assign';
-import {Descriptor, Stamp} from './interface';
+'use strict'
+import {
+  COMPOSE,
+  DEEP_PROPERTIES,
+  INITIALIZERS,
+  METHODS,
+  PROPERTIES,
+  PROPERTY_DESCRIPTORS
+} from './const'
+import { isFunction } from './utils'
+import merge from 'lodash/merge'
+import assign from 'lodash/assign'
+import { Descriptor, Stamp } from './interface'
 
 export default function createFactory() {
-
   /**
    * The Stamp factory function
    * @typedef {Function} Stamp
+   * @param {Descriptor} options
    * @returns {*} Instantiated object
    * @property {Descriptor} compose - The Stamp descriptor and composition function
    */
   return function Stamp(options: Descriptor): {} {
-    let i: any = (Stamp as Stamp)[COMPOSE] || {};
+    let i: any = (Stamp as Stamp)[COMPOSE] || {}
     // Next line was optimized for most JS VMs. Please, be careful here!
-    let obj: {} = {__proto__: i[METHODS]};
+    let obj: {} = { __proto__: i[METHODS] }
 
-    let inits: Function[] = i[INITIALIZERS];
-    let args = Array.prototype.slice.apply(arguments);
+    let inits: Function[] = i[INITIALIZERS]
+    let args = Array.prototype.slice.apply(arguments)
 
-    let tmp = i[DEEP_PROPERTIES];
-    if (tmp) merge(obj, tmp);
-    tmp = i[PROPERTIES];
-    if (tmp) assign(obj, tmp);
-    tmp = i[PROPERTY_DESCRIPTORS];
-    if (tmp) Object.defineProperties(obj, tmp);
+    let tmp = i[DEEP_PROPERTIES]
+    if (tmp) merge(obj, tmp)
+    tmp = i[PROPERTIES]
+    if (tmp) assign(obj, tmp)
+    tmp = i[PROPERTY_DESCRIPTORS]
+    if (tmp) Object.defineProperties(obj, tmp)
 
-    if (!inits || !inits.length) return obj;
+    if (!inits || !inits.length) return obj
 
-    if (options === undefined) options = {};
-    for (i = 0; i < inits.length;) {
-      let initializer = inits[i++];
+    if (options === undefined) options = {}
+    for (i = 0; i < inits.length; ) {
+      let initializer = inits[i++]
       if (isFunction(initializer)) {
-        let returnedValue = initializer.call(obj, options,
-          {instance: obj, stamp: Stamp, args: args});
-        obj = returnedValue === undefined ? obj : returnedValue;
+        let returnedValue = initializer.call(obj, options, {
+          instance: obj,
+          stamp: Stamp,
+          args: args
+        })
+        obj = returnedValue === undefined ? obj : returnedValue
       }
     }
 
-    return obj;
-  };
+    return obj
+  }
 }
